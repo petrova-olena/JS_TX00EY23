0a8a7764dbc843ac8ae852b93cb41f860f31b968
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-03-21 19:41:18 +0200

    Add JS Recap2 Task5

--- JS_recap2/t4/task4.js
+++ JS_recap2/t4/task4.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Function that sorts an array
+function sortArray(array) {
+  const newArray = array.sort((a, b) => a - b);
+  return newArray;
+}
+
+// Hard-coded array, sorted and printed
+const testArray = [10, 5, 43, 1, 9, 3];
+sortArray(testArray);
+
+console.log('Result: ' + testArray);
--- JS_recap2/t5/task5.js
+++ JS_recap2/t5/task5.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Function that sorts an array
+function sortArray(numbers, order) {
+  let newArray = [];
+  if (order === 'asc') {
+    newArray = numbers.sort((a, b) => a - b);
+  } else if (order === 'desc') {
+    newArray = numbers.sort((a, b) => b - a);
+  }
+  return newArray;
+}
+
+// Hard-coded array, sorted and printed
+const numbers = [5, 2, 8, 1, 9];
+
+console.log(sortArray(numbers, 'asc'));
+console.log(sortArray(numbers, 'desc'));
