656c15243246ea43e786b4f512fcaeda65ae0cd2
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-03-21 20:17:18 +0200

    Add JS Recap2 Task6

--- JS_recap2/t6/task6.js
+++ JS_recap2/t6/task6.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// Create empty list
+let movies = [];
+
+// Add needed amount of movies to the list
+const amount = Number(prompt('Enter amount of movies:'));
+for (let i = 0; i < amount; i++) {
+  const title = prompt('Enter title:');
+  const rating = Number(prompt('Enter rating of ' + title + ':'));
+
+  const movie = {
+    title: title,
+    rating: rating,
+  };
+
+  movies.push(movie);
+}
+
+// Sort movies in descendent way
+movies.sort(function (a, b) {
+  return b.rating - a.rating;
+});
+
+console.log(movies);
+
+// Output the best movie and list of movies
+document.querySelector('#best').innerHTML = movies[0].title;
+
+for (const movie of movies) {
+  document.querySelector('#target').innerHTML +=
+    `<li>${movie.title}, ${movie.rating}</li>`;
+}
